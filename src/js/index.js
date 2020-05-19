64a858ad27047965c42bca4c52082a464976d764
rarlala <wtw9611@gmail.com>
2020-05-19 16:10:20 +0900

    header, visual 작업 영역까지 commit

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+$(function() {
+
+    //     $(".gnb > ul>li")
+    //         .on('mouseover', function() {
+    //             $('header .gnb-area').css({
+    //                 'position': 'absolute',
+    //                 'background': '#fff',
+    //                 'height': '430px',
+    //             })
+    //         })
+
+    //     $(".gnb-list")
+    //         .on('mouseout', function() {
+    //             $('header .gnb-area').css({
+    //                 'position': 'relative',
+    //                 'background': '#67625D',
+    //                 'height': '60px'
+    //             })
+    //         })
+
+
+    // 슬라이드
+
+
+
+})
